1249168ccae2c2e5da718fcc0632bb24a6b345c5
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-03-29 16:22:18 +0200

    adding odd or even number

--- js/script.js
+++ js/script.js
@@ -7,6 +7,66 @@
 /**/
 
 
+/**
+ *
+ *  TASK TWO: ODD OR EVEN NUMBER
+ *  1. ASK THE USER TO CHOOSE BETWEEN ODD OR EVEN
+ *  2. ASK THE USER TO INSERT A NUMBER BETWEEN 1 & 5
+ *  3. GENERATE A RANDOM NUMBER, BETWEEN 1 & 5, FOR THE COMPUTER (USE A FUNCTION)
+ *  4. SUM THE TWO NUMBERS
+ *  5. UNDERSTAND IF THE SUM IS ODD OR EVEN (USE A FUNCTION)
+ *  6. DECLARE WHO WINS
+ * 
+/**/
+
+// VARIABLES
+
+var userDecision = prompt('Decide between: Odd or Even?').toLowerCase().trim();
+console.log(userDecision);
+
+var userNumber = parseInt( prompt('Choose a Number between 1 and 5'));
+console.log('The number chosen from the user is: ' + userNumber);
+var computerNumber = randomNumber(1, 5);
+console.log('The number chosen from the computer is: ' + computerNumber);
+
+// EVENTS
+
+var sum = userNumber + computerNumber;
+console.log('The sum of both number is: ' + sum);
+
+var result = oddOrEven(sum);
+console.log('The sum is ' + result);
+
+if(result === userDecision) {
+    console.log('User Wins!!!');
+} else {
+    console.log('Computer Wins!');
+}
+
+/**
+ * 
+ * UTILITIES 
+ * 
+/**/
+
+function randomNumber(min, max) {
+    var randomNumber = Math.floor(Math.random() * max - min + 1) + min;
+
+    return randomNumber;
+}
+
+function oddOrEven(number) {
+    
+    if(number % 2 === 0) {
+        return 'even';
+    }
+
+    return 'odd';
+}
+
+
+
+
 
 /**
  * 
@@ -17,7 +77,7 @@
 /**/
 
 // VARIABLE
-
+/*
 var userWord = prompt('Please enter a word here').toLowerCase().trim();
 console.log(userWord);
 
@@ -46,6 +106,7 @@ switch(userWord === isPalindromes) {
  * @param {word} word // TO UNDERSTAND IF THE WORD IS PALINDROME
  * @returns isPalindromes
  */
+/*
 function isPalindrome(word) {
     var isPalindromes = '';
 
@@ -56,3 +117,4 @@ function isPalindrome(word) {
 
     return isPalindromes;
 }
+*/
